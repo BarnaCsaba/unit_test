14b0e151462c5ddfa80a564b07641f0638c48cfe
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2025-11-14 08:35:11 +0100

    Update test_calculator.py

--- calculator_test/test_calculator.py
+++ calculator_test/test_calculator.py
@@ -9,3 +9,14 @@ def test_substract():
     assert substract(5, 3) == 2
     assert substract(0, 0) == 0
     assert substract(-1, -1) == 0   
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-2, 3) == -6
+    assert multiply(0, 5) == 0
+
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-6, 3) == -2
+    with pytest.raises(ValueError):
+        divide(5, 0)
